4ff2c907e22db4ac5459c7180933e11c354a4546
oaksoeaung-dev <oaksoeaung.dev@gmail.com>
2024-08-01 18:02:50 +0630

    Add login and register

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,26 +1,30 @@
 // Toggle Menu Button
 let menuToggle = document.querySelector(".toggle");
 menuToggle.addEventListener("click", () => {
-  document.getElementById("line1").classList.toggle("w-[25px]");
-  document.getElementById("line1").classList.toggle("w-[25px]");
-  document.getElementById("line1").classList.toggle("rotate-45");
-  document.getElementById("line1").classList.toggle("translate-y-[0px]");
-  document.getElementById("line1").classList.toggle("-translate-y-[8px]");
+    document.getElementById("line1").classList.toggle("w-[25px]");
+    document.getElementById("line1").classList.toggle("w-[25px]");
+    document.getElementById("line1").classList.toggle("rotate-45");
+    document.getElementById("line1").classList.toggle("translate-y-[0px]");
+    document.getElementById("line1").classList.toggle("-translate-y-[8px]");
 
-  document.getElementById("line2").classList.toggle("translate-x-[60px]");
+    document.getElementById("line2").classList.toggle("translate-x-[60px]");
 
-  document.getElementById("line3").classList.toggle("w-[25px]");
-  document.getElementById("line3").classList.toggle("w-[15px]");
-  document.getElementById("line3").classList.toggle("rotate-[315deg]");
-  document.getElementById("line3").classList.toggle("translate-y-[0px]");
-  document.getElementById("line3").classList.toggle("translate-y-[8px]");
+    document.getElementById("line3").classList.toggle("w-[25px]");
+    document.getElementById("line3").classList.toggle("w-[15px]");
+    document.getElementById("line3").classList.toggle("rotate-[315deg]");
+    document.getElementById("line3").classList.toggle("translate-y-[0px]");
+    document.getElementById("line3").classList.toggle("translate-y-[8px]");
 });
 
-// Scroll fix nav
-// window.addEventListener("scroll", function () {
-//     let nav = document.querySelector("nav");
-//     nav.classList.toggle("fixed", window.scrollY > 100);
-//     nav.classList.toggle("top-0", window.scrollY > 100);
-//     nav.classList.toggle("inset-x-0", window.scrollY > 100);
-//     nav.classList.toggle("animate-fade-in", window.scrollY > 100);
-// });
+// Login And Register
+const container = document.getElementById("container");
+const registerBtn = document.getElementById("register");
+const loginBtn = document.getElementById("login");
+
+registerBtn.addEventListener("click", () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener("click", () => {
+    container.classList.remove("active");
+});
